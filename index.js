b1fa236b579e288f62e9df846aaa053d6e02fe50
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2023-11-04 01:04:41 +0530

    add rbac

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 const fs = require('fs');
 
 
-const configMapName = 'vaultrole';
-const namespace = 'dev'; // Replace with your ConfigMap's namespace
+const configMapName = process.env.CM_NAME;
+const namespace = process.env.NAMESPACE; // Replace with your ConfigMap's namespace
 const https = require('https');
 
 const agent = new https.Agent({
@@ -59,7 +59,9 @@ console.log("Started Job Successfull");
 
 for (let index = 0; index < 10; index++) {
     console.log('Hello, this is kubeos-vault-sync created by my developer portal!');  
-    fetchConfigMap();
+    var cmData = fetchConfigMap();
+    console.log(cmData);
+    console.log(index);
 }
 
 

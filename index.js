f0950ce4e13c9492300a999a7cdb4380152ee4b0
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2023-11-03 23:04:24 +0530

    add jobs

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-var index = 0;
+const configMapName = 'vaultrole';
+const namespace = 'dev'; // Replace with your ConfigMap's namespace
 
 for (let index = 0; index < 10; index++) {
     console.log('Hello, this is kubeos-vault-sync created by my developer portal!');  
+    fetchConfigMap();
 }
 
 console.log("Ending Job Successfull");
+
+
+const fetchConfigMap = async () => {
+    try {
+        const token = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf8');
+        const api = `https://kubernetes.default.svc/api/v1/namespaces/${namespace}/configmaps/${configMapName}`;
+
+        const response = await axios({
+            method: 'get',
+            url: api,
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching ConfigMap:', error);
+        return null;
+    }
+};
+
+const updateConfigMap = async (configMapData) => {
+    try {
+        const token = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf8');
+        const api = `https://kubernetes.default.svc/api/v1/namespaces/${namespace}/configmaps/${configMapName}`;
+
+        await axios({
+            method: 'put',
+            url: api,
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            data: configMapData,
+        });
+
+        console.log('ConfigMap updated');
+    } catch (error) {
+        console.error('Error updating ConfigMap:', error);
+    }
+};

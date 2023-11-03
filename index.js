fc0554dcd3aba7f154a64334999c907db16a7917
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2023-11-04 02:14:16 +0530

    vault onboarder

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const fs = require('fs');
 
 const configMapName = process.env.CM_NAME;
 const namespace = process.env.NAMESPACE; // Replace with your ConfigMap's namespace
+
+const VAULT_TOKEN = process.env.TOKEN; // Replace with your Vault token
+const VAULT_ADDR = 'vault-0.vault-backend.svc:8200'; // Replace with your Vault address
+
+
+
 const https = require('https');
 
 const agent = new https.Agent({
@@ -64,5 +70,46 @@ for (let index = 0; index < 10; index++) {
     console.log(index);
 }
 
+function OnboardAppToVault(appname) {
+    
+    const policyurl = `${VAULT_ADDR}/v1/sys/policy/${appname}`; // Replace 'tester' with the policy name
+    
+    const config = {
+        headers: {
+          'X-Vault-Token': VAULT_TOKEN,
+          'X-Vault-Namespace': 'admin', // Adjust the namespace accordingly
+          'Content-Type': 'application/json',
+        }
+      };
+
+    const policy_payload ="{\"policy": "# Grant 'create' and 'update' permission to paths prefixed by 'kubeos'\npath \"kubeos/*\" {\n  capabilities = [ \"create\", \"read\", \"update\", \"delete\", \"list\" ]\n}\n\n# Manage namespaces\npath \"kubeos/dev/*\" {\n   capabilities = [ \"create\", \"read\", \"update\", \"delete\", \"list\" ]\n}\n}"
+    
+    axios.post(url, policy_payload, config)
+    .then(response => {
+        console.log('Policy created:', response.data);
+    })
+    .catch(error => {
+        console.error('Error creating policy:', error.response.data);
+    });
+
+
+    const roleurl = `${VAULT_ADDR}/v1/auth/kubernetes/role/${appname}`;
+
+    var role_payload = {
+        "bound_service_account_names": appname,
+        "bound_service_account_namespaces": "dev",
+        "policies": ["dev", "prod"],
+        "max_ttl": 1800000
+      }
+
+      axios.post(roleurl, role_payload, config)
+      .then(response => {
+          console.log('Policy created:', response.data);
+      })
+      .catch(error => {
+          console.error('Error creating policy:', error.response.data);
+      });
+  
+}
 
 console.log("Ending Job Successfull");

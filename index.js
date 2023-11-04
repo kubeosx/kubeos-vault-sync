926ae82df4721c5d3050cb9ed4b15f1f34a4b9b8
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2023-11-04 15:12:40 +0530

    add capabilities

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const fs = require('fs');
 const configMapName = process.env.CM_NAME;
 const namespace = process.env.NAMESPACE; // Replace with your ConfigMap's namespace
 
-const VAULT_TOKEN = process.env.TOKEN; // Replace with your Vault token
-const VAULT_ADDR = 'vault-0.vault-backend.svc:8200'; // Replace with your Vault address
+const VAULT_TOKEN = process.env.VAULT_TOKEN; // Replace with your Vault token
+const VAULT_ADDR = process.env.VAULT_ADDR;; // Replace with your Vault address
 
 
 
@@ -16,7 +16,7 @@ const https = require('https');
 const agent = new https.Agent({
     ca: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/ca.crt')
 });
-  
+
 
 const fetchConfigMap = async () => {
     try {
@@ -33,6 +33,7 @@ const fetchConfigMap = async () => {
             },
         });
         console.log(response.data);
+        OnboardAppToVault(appname)
         return response.data;
     } catch (error) {
         console.error('Error fetching ConfigMap:', error);
@@ -64,33 +65,39 @@ const updateConfigMap = async (configMapData) => {
 console.log("Started Job Successfull");
 
 for (let index = 0; index < 10; index++) {
-    console.log('Hello, this is kubeos-vault-sync created by my developer portal!');  
+    console.log('Hello, this is kubeos-vault-sync created by my developer portal!');
     var cmData = fetchConfigMap();
     console.log(cmData);
-    console.log(index);
+    let apps = JSON.parse(cmData.data);
+    for (let index = 0; index < apps.length; index++) {
+        const element = apps[index];
+        console.log(element);
+        OnboardAppToVault(element);
+    }
+
 }
 
 function OnboardAppToVault(appname) {
-    
+
     const policyurl = `${VAULT_ADDR}/v1/sys/policy/${appname}`; // Replace 'tester' with the policy name
-    
+
     const config = {
         headers: {
-          'X-Vault-Token': VAULT_TOKEN,
-          'X-Vault-Namespace': 'admin', // Adjust the namespace accordingly
-          'Content-Type': 'application/json',
+            'X-Vault-Token': VAULT_TOKEN,
+            'X-Vault-Namespace': 'admin', // Adjust the namespace accordingly
+            'Content-Type': 'application/json',
         }
-      };
+    };
 
-    const policy_payload ="{\"policy": "# Grant 'create' and 'update' permission to paths prefixed by 'kubeos'\npath \"kubeos/*\" {\n  capabilities = [ \"create\", \"read\", \"update\", \"delete\", \"list\" ]\n}\n\n# Manage namespaces\npath \"kubeos/dev/*\" {\n   capabilities = [ \"create\", \"read\", \"update\", \"delete\", \"list\" ]\n}\n}"
-    
+    const policy_payload = "{\"policy": "# Grant 'create' and 'update' permission to paths prefixed by 'kubeos'\npath \"kubeos/*\" {\n  capabilities = [ \"create\", \"read\", \"update\", \"delete\", \"list\" ]\n}\n\n# Manage namespaces\npath \"kubeos/dev/*\" {\n   capabilities = [ \"create\", \"read\", \"update\", \"delete\", \"list\" ]\n}\n}"
+
     axios.post(url, policy_payload, config)
-    .then(response => {
-        console.log('Policy created:', response.data);
-    })
-    .catch(error => {
-        console.error('Error creating policy:', error.response.data);
-    });
+        .then(response => {
+            console.log('Policy created:', response.data);
+        })
+        .catch(error => {
+            console.error('Error creating policy:', error.response.data);
+        });
 
 
     const roleurl = `${VAULT_ADDR}/v1/auth/kubernetes/role/${appname}`;
@@ -100,16 +107,16 @@ function OnboardAppToVault(appname) {
         "bound_service_account_namespaces": "dev",
         "policies": ["dev", "prod"],
         "max_ttl": 1800000
-      }
+    }
 
-      axios.post(roleurl, role_payload, config)
-      .then(response => {
-          console.log('Policy created:', response.data);
-      })
-      .catch(error => {
-          console.error('Error creating policy:', error.response.data);
-      });
-  
+    axios.post(roleurl, role_payload, config)
+        .then(response => {
+            console.log('Policy created:', response.data);
+        })
+        .catch(error => {
+            console.error('Error creating policy:', error.response.data);
+        });
+
 }
 
 console.log("Ending Job Successfull");
